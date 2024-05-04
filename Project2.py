9687f3b5d4d83d2a09026a42256829ea7623d48d
Deep15125 <deepgupta15125@gmail.com>
2024-05-04 23:26:37 +0530

    This is Voice Recorder

---
+++ Project2.py
@@ -0,0 +1,16 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as wv
+
+freq = 44100
+duration = int(input("Enter the Duration of Recording"))
+
+recording = sd.rec(int(duration * freq),samplerate=freq, channels=2)
+
+print("Record Your Recording.....")
+
+sd.wait()
+write("Rec.wav", freq, recording)
+
+print("Completed... Your recording is saved as Rec")
+print("Thank You")
