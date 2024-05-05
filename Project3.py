fc7cd4f605b5e79e4ffe6e3cb885d58790e20fad
Deep15125 <deepgupta15125@gmail.com>
2024-05-05 09:08:49 +0530

    It is Create Payment Reciept

---
+++ Project3.py
@@ -0,0 +1,38 @@
+from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, TableStyle
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
+
+MYDATA = [
+            [ "S.NO." , "DATE OF PURCHASING" , "NAME OF COURSE" , "SUBSCRIPTION TIME" , "PRICE OF COURSE(RS.)"],
+            ["01" , "01/05/2024" , "PYTHON PROGRAMMING" , "LIFETIME" , "2,000"],
+            ["02" , "05/05/2024" , "FULL STACK DEVELOPMENT" , "1 YEAR" , "5,000"],
+            ["SUBTOTAL" , "" , "" , "" , "7,000"],
+            ["DISCOUNT" , "" , "" , "" , "700"],
+            ["TOTAL" , "" , "" , "" , "6,300"],
+]
+
+pdf = SimpleDocTemplate( "reciept.pdf" , papersize = A4)
+
+styles = getSampleStyleSheet()
+
+title_style = styles[ "Heading1" ]
+
+title_style.alignment = 1
+
+title = Paragraph( "Internshalla" , title_style )
+
+style = TableStyle(
+    [
+        ( "BOX" , ( 0, 0), (-1,-1), 1 , colors.black),  
+        ( "GRID" , ( 0, 0), (4,4), 1 , colors.black),
+        ( "BACKGROUND" , ( 0, 0), (3,0), colors.green),
+        ( "TEXTCOLOR" , ( 0, 0), (-1,0), colors.black),
+        ( "ALIGN" , ( 0, 0), (-1,-1), "CENTER" , "BOLD"),
+        ( "BACKGROUND" , ( 0, 1), (-1,-1), colors.palegreen)
+    ]
+)
+
+table = Table( MYDATA , style = style )
+
+pdf.build([ title , table])
--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This is CipherByte Technologies Projects.
 My Project1 is Rock/Stone Paper Scissor Game which is built by Python.
 <br>
 My project 2 is Voice Recorder which is also built by Python.
+<br>
+My project 3 is Create a Payment reciept which is built by Python.
